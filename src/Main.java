63c7898f685d2410072e9d518c725b2c2d1ac613
Elena <elenela@yandex.ru>
2023-04-19 14:20:43 +0300

    Добрый день! Проверьте, пожалуйста, ДЗ.

---
+++ src/Main.java
@@ -0,0 +1,101 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задание 2");
+
+        var dog1 = 8.0+4;
+        var cat1 = 3.6+4;
+        var paper1 = 763789+4;
+        System.out.println(dog1);
+        System.out.println(cat1);
+        System.out.println(paper1);
+
+        System.out.println("Задание 3");
+
+
+        var dog2 = 8.0+4-3.5;
+        var cat2 = 3.6+4-1.6;
+        var paper2 = 763789+4-7639;
+        System.out.println(dog2);
+        System.out.println(cat2);
+        System.out.println(paper2);
+
+        System.out.println("Задание 4");
+        var friend  = 19;
+        var increaseBy2 = 19 + 2;
+        var divisionBy7 = increaseBy2 / 7;
+        System.out.println(friend);
+        System.out.println(increaseBy2);
+        System.out.println(divisionBy7);
+
+        System.out.println("Задание 5");
+
+        var frog = 3.5;
+        var anIncreaseOf10 = 3.5 * 10;
+        var divisionBy3WithHalf   = anIncreaseOf10 / 3.5;
+        var increaseBy4 =  divisionBy3WithHalf + 4;
+        System.out.println(frog);
+        System.out.println(anIncreaseOf10);
+        System.out.println(divisionBy3WithHalf);
+        System.out.println(increaseBy4);
+
+        System.out.println("Задание 6");
+
+        var oneBoxer = 78.2;
+        var secondBoxer =  82.7;
+        var totalWeightOfTwoFighters = oneBoxer + secondBoxer;
+        var differenceBetweenTheWeightsOfTheFighters = secondBoxer - oneBoxer;
+        System.out.println(oneBoxer);
+        System.out.println(secondBoxer);
+        System.out.println(totalWeightOfTwoFighters);
+        System.out.println(differenceBetweenTheWeightsOfTheFighters);
+
+        System.out.println("Задание 7");
+
+        var differenceInTheWeightsOfAthletesFirstWay = secondBoxer - oneBoxer;
+        var differenceInTheWeightsOfAthletesSecondWay = secondBoxer % oneBoxer;
+        System.out.println(differenceInTheWeightsOfAthletesFirstWay);
+        System.out.println(differenceInTheWeightsOfAthletesSecondWay);
+
+        System.out.println("Задание 8");
+
+        var totalNumberOfHours = 640;
+        var timeOfEachEmployee = 8;
+        var totalEmployees = totalNumberOfHours / timeOfEachEmployee;
+        System.out.println("Всего работников в компании - " + totalEmployees + "  человек");
+
+        System.out.println("Задание 8 (2)");
+
+        var increaseBy94TotalEmployees =  totalEmployees  + 94;
+        var totalTimeIfIncreaseBy94Employees =  increaseBy94TotalEmployees * timeOfEachEmployee;
+        System.out.println(increaseBy94TotalEmployees);
+        System.out.println("Если в компании работает - " + increaseBy94TotalEmployees + "  человека," +
+                        " то всего " + totalTimeIfIncreaseBy94Employees +  " часов работы может быть поделено между сотрудниками"
+                );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
